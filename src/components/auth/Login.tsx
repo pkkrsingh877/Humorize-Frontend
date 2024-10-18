cec9dbf1ccf9cdb790e5b278f578907d59c377f4
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-10-18 07:23:28 +0530

    Login, Signup Working

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,43 +1,68 @@
 import { Box, TextField, FormControl, Typography, Button } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const userData = { email, password };
+
+        try {
+            const response = await axios.post('http://localhost:5000/api/auth/login', userData);
+            if (response) {
+                console.log('User logged in successfully:', response.data);
+            }
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <Box
             component="form"
             noValidate
             autoComplete="off"
+            onSubmit={handleSubmit}
         >
-            <form method="POST" action="http://localhost:5000/api/auth/login">
-                <FormControl sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    gap: '1.5rem',
-                    padding: '5rem'
-                }}>
-                    <Typography variant="h3" component="h3" sx={{ textAlign: 'center' }}>
-                        Login
-                    </Typography>
-                    <TextField
-                        id="outlined"
-                        label="Email"
-                        type="email"
-                        required
-                    />
-                    <TextField
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        required
-                    />
-                    <Button variant="contained" startIcon={<LoginIcon />}>
-                        Login
-                    </Button>
-                </FormControl>
-            </form>
+            <FormControl sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                gap: '1.5rem',
+                padding: '5rem'
+            }}>
+                <Typography variant="h3" component="h3" sx={{ textAlign: 'center' }}>
+                    Login
+                </Typography>
+                <TextField
+                    id="outlined"
+                    label="Email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <Button variant="contained" startIcon={<LoginIcon />} type="submit">
+                    Login
+                </Button>
+            </FormControl>
         </Box>
     );
 }
--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,49 +1,78 @@
 import { Box, TextField, FormControl, Typography, Button } from '@mui/material';
 import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Submit event fired")
+
+        const userData = { name, email, password };
+
+        try {
+            const response = await axios.post('http://localhost:5000/api/auth/signup', userData);
+            if (response) {
+                console.log('User signed up successfully:', response.data);
+            }
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <Box
             component="form"
             noValidate
             autoComplete="off"
+            onSubmit={handleSubmit}
         >
-            <form action="http://localhost:500/api/auth/signup" method="POST">
-                <FormControl sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    gap: '1.5rem',
-                    padding: '5rem'
-                }}>
-                    <Typography variant="h3" component="h3" sx={{ textAlign: 'center' }}>
-                        Signup
-                    </Typography>
-                    <TextField
-                        id="outlined"
-                        label="Name"
-                        type="text"
-                        required
-                    />
-                    <TextField
-                        id="outlined"
-                        label="Email"
-                        type="email"
-                        required
-                    />
-                    <TextField
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        required
-                    />
-                    <Button variant="contained" startIcon={<AppRegistrationIcon />}>
-                        Signup
-                    </Button>
-                </FormControl>
-            </form>
+            <FormControl sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                gap: '1.5rem',
+                padding: '5rem'
+            }}>
+                <Typography variant="h3" component="h3" sx={{ textAlign: 'center' }}>
+                    Signup
+                </Typography>
+                <TextField
+                    id="outlined"
+                    label="Name"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+                <TextField
+                    id="outlined"
+                    label="Email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    value={password}
+                    autoComplete="current-password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <Button variant="contained" startIcon={<AppRegistrationIcon />} type='submit'>
+                    Signup
+                </Button>
+            </FormControl>
         </Box>
     );
 }
